bac97f91d6ae275afe029fe8fb6a23a7d3dad1a0
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-20 10:44:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
-
+const playstore = new Discord.Client();
 const config = require("./config.json");
 
-bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
+playstore.on('ready', () => {
+  console.log(`Bot has started, with ${playstore.users.size} users, in ${playstore.channels.size} channels of ${playstore.guilds.size} guilds.`);
+  console.log(`Logged in as ${playstore.user.tag}!`);
+  console.log(`PlayStores is online`);
 
-bot.user.setPresence({game: {name: 'prefix: = | =help', type:0 } });
+playstore.user.setGame(`=h | I has ${playstore.guilds.size} Servers`);
 });
-
-bot.on('message', message => {
+ // Prefix settings
+playstore.on('message', message => {
   if(message.author.bot) return;
   if(!message.content.startsWith(config.prefix)) return;
 
@@ -17,30 +18,38 @@ bot.on('message', message => {
   command = command.slice(config.prefix.length);
 
   let args = message.content.split(" ").slice(1);
-
+  // Add Command
   if (command === "add") {
     let numArry = args.map(n=> parseInt(n));
     let total = numArry.reduce( (p, c) => p+c);
 
     message.channel.sendMessage(total);
-  } 
 
+  }
+  
+  if (command === "h") {
+    message.reply("Sending.. Sending.. Sending...");
+    message.author.sendMessage("List of commands:");
+    message.author.sendMessage("``=say (Says what you tell it to.)``");
+    message.author.sendMessage("``=avatar (Posts a pic of your profile pic.)``");
+    message.author.sendMessage("``=ping (Shows how fast the bot is.)``");
+    message.reply("Help has been sent!");
+  }
+  // list of shit
   if (command === "say") {
     message.channel.sendMessage(args.join(" "));
-  } 
+  }
 
-  if (command === "invite") {
-    message.channel.sendMessage("Invite: https://discord.gg/r4aBuTw");
-  } 
-
-  if (command === "donate") {
-    message.channel.sendMessage("Help CriticalGamers stay online! | paypal.me/ServerSupport |");
-  } 
-  
-  if (command === "help") {
-	 message.channel.sendMessage("Commands: say, donate | do =invite for support |");
+  }
+  // Shows persons profile picture
+  if (command === "avatar") {
+    message.reply(message.author.avatarURL);
+  }
+  // Working ping code
+  if (command === "ping") {
+      message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
   }
   
 });
-
-bot.login(process.env.BOT_TOKEN);
+// Token for bot to run
+playstore.login(process.env.BOT_TOKEN);
